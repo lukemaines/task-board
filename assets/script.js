a02beed717e7fa01cc87d8548f90ce2401ef37ae
l.maines <lmmaines28@gmail.com>
2024-05-28 20:07:32 -0400

    handled generating task id

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+return nextId++;
 }
 
 // Todo: create a function to create a task card
