4d446a58005f975e0478b167e2885208d271b44e
l.maines <lmmaines28@gmail.com>
2024-06-23 17:53:18 -0400

    added createTaskCard function

--- assets/script.js
+++ assets/script.js
@@ -4,26 +4,50 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-return nextId++;
+    return nextId++;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const deadline = dayjs(task.deadline); 
+    const today = dayjs();
+    let cardColor = '';
+
+    if (task.status === 'done') {
+        cardColor = "text-bg-light";
+    }
+    else if (today.isSame(deadline, 'day')) {
+        cardColor = "text-bg-danger";
+    }
+    else (today.isBefore(deadline, 'day')) {
+        cardColor = "text-bg-warning"; 
+    }
+    
+    return `
+    <div class="card ${cardColor} mb-3" style="max-width: 18rem;" data-id="${task.id}">
+        <div class="card-body">
+            <h5 class="card-title">${task.title}</h5>
+            <p class="card-text">Due: ${task.dueDate}/p>
+            <a href="#" class="btn btn-danger delete-task">Delete</a>
+        </div>
+    </div>`;
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    $('#todo').empty();
+    $('#in-progress').empty();
+    
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
